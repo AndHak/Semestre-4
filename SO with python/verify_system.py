13cf718ca2416de7de08f1ed81bf10d24ef242be
AndHak <andresfg13789@gmail.com>
2024-07-07 18:40:24 -0500

    act

---
+++ SO with python/verify_system.py
@@ -0,0 +1,16 @@
+import psutil
+import shutil
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("Take care of your computer health!")
+else:
+    print("Everything is ok!") 
