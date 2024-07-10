f61c2b72eb22e59b079f113df1ae09fef6f13b89
AndHak <andresfg13789@gmail.com>
2024-07-10 03:08:23 -0500

    test

---
+++ SO with python/rearrange_name_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from rearrange_name import rearrange_name
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+#Run the tests
+unittest.main()
+
